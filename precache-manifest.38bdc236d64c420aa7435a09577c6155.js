336ab58a494f22d3012c8541996f1aa1ab395427
Maciek <maciej.gorski995@gmail.com>
2020-05-14 20:23:20 +0200

    Updates

---
+++ precache-manifest.38bdc236d64c420aa7435a09577c6155.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "686a4cd7eb5a615c60cada31953bc18d",
+    "url": "/hot16challenge2/index.html"
+  },
+  {
+    "revision": "dd93ad10f57d0da828c9",
+    "url": "/hot16challenge2/static/css/main.f89dd071.chunk.css"
+  },
+  {
+    "revision": "d06dfa2406b31e911e4f",
+    "url": "/hot16challenge2/static/js/2.94eca825.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hot16challenge2/static/js/2.94eca825.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd93ad10f57d0da828c9",
+    "url": "/hot16challenge2/static/js/main.0a08299d.chunk.js"
+  },
+  {
+    "revision": "0bdaa74016a5ca742e21",
+    "url": "/hot16challenge2/static/js/runtime-main.918a7fde.js"
+  }
+]);
